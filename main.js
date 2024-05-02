a3a29e980f16f3ffdeb371aad2ecf38b91b53fb8
Juan Pablo <juanpablolc414@gmail.com>
2024-05-02 17:43:47 -0500

    avance

---
+++ main.js
@@ -0,0 +1,30 @@
+let newX = 0, newY = 0, oldX = 0, oldY = 0;
+
+const card = document.getElementById("card");
+
+card.addEventListener("mousedown", mpuseDown);
+
+function mpuseDown(e){
+    oldX = e.clientX;
+    oldY = e.clientY;
+
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+}
+
+function mouseMove(e){
+    newX = e.clientX - oldX;
+    newY = e.clientY - oldY;
+
+    card.style.top = card.offsetTop + newY + "px";
+    card.style.left = card.offsetLeft + newX + "px";
+
+    oldX = e.clientX;
+    oldY = e.clientY;
+
+    console.log(newX, newY);
+    console.log(oldX, oldY);
+}
+
+function mouseUp(e){}
